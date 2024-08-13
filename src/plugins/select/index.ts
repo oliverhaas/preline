5013a2d02d82b284f9b83648c35ff4f2fe015e9e
Oliver Haas <oliver.sebastian.haas@gmail.com>
2024-08-13 21:16:02 +0200

    feat: better autoInit for ajax, fix: memory leak autoInit

--- src/plugins/select/index.ts
+++ src/plugins/select/index.ts
@@ -1099,10 +1099,21 @@ class HSSelect extends HSBasePlugin<ISelectOptions> implements ISelect {
 			: null;
 	}
 
-	static autoInit() {
-		if (!window.$hsSelectCollection) window.$hsSelectCollection = [];
+	static autoInit(target: HTMLElement | null = null) {
+		if (!window.$hsSelectCollection) {
+            window.$hsSelectCollection = [];
+            window.addEventListener('click', (evt) => {
+				const evtTarget = evt.target;
 
-		document
+				HSSelect.closeCurrentlyOpened(evtTarget as HTMLElement);
+			});
+
+			document.addEventListener('keydown', (evt) =>
+				HSSelect.accessibility(evt),
+			);
+        }
+
+		(target || document)
 			.querySelectorAll('[data-hs-select]:not(.--prevent-on-load-init)')
 			.forEach((el: HTMLElement) => {
 				if (
@@ -1116,18 +1127,6 @@ class HSSelect extends HSBasePlugin<ISelectOptions> implements ISelect {
 					new HSSelect(el, options);
 				}
 			});
-
-		if (window.$hsSelectCollection) {
-			window.addEventListener('click', (evt) => {
-				const evtTarget = evt.target;
-
-				HSSelect.closeCurrentlyOpened(evtTarget as HTMLElement);
-			});
-
-			document.addEventListener('keydown', (evt) =>
-				HSSelect.accessibility(evt),
-			);
-		}
 	}
 
 	static open(target: HTMLElement | string) {
